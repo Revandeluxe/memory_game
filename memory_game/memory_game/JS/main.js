e84cdc5a76baab9dc1b9d31f254e8c99ebf6f5a6
Evan Bauer <33360771+Revandeluxe@users.noreply.github.com>
2017-11-13 14:13:02 -0800

    Add files via upload

--- memory_game/memory_game/JS/main.js
+++ memory_game/memory_game/JS/main.js
@@ -1,26 +1,25 @@
-console.log("Up and Running!");
-
 var cards = ["queen", "queen", "king", "king"];
-
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function (){
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	} else {alert ("sorry, try again");
+		}
+	}	
+	
 
-cardsInPlay.push('cardOne');
-
-console.log("User flipped" + cardOne);
-
-var cardTwo = cards[1];
-
-cardsInPlay.push('cardTwo')
-
-console.log("User Flipped" + cardTwo)
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You Found a Match!");
-} else {
-	alert("Sorry, Try again");
+var flipCard = function(cardID){
+	console.log("User flipped" + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
+
+
+flipCard(0);
+flipCard(2);
 
 
 
